9c51f4bb4fcc3d683c9fc6a3894e57a0d4d7f078
RadisolFigueiredo <radisolfigueiredo@gmail.com>
2019-02-27 12:12:33 -0300

    about us responsivo ok

--- de-amor-client/src/components/aboutUs/AboutUs.js
+++ de-amor-client/src/components/aboutUs/AboutUs.js
@@ -1,16 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./AboutUs.css";
+import { Link } from "react-router-dom";
+import NavDeslog from "../navDeslog/NavDeslog";
 
-class AboutUs extends Component {	
-	render () {
-		return (
-		<div>
-
-		</div>
-
-		);
-
-	}
+class AboutUs extends Component {
+  render() {
+    return (
+      <div id="about">
+        <NavDeslog />
+        <div id="background-AboutUs">
+          <div className="title-about">
+            <h1>Quem Somos</h1>
+          </div>
+          <div className="text-about">
+            <p>
+              O DÊ AMOR é uma plataforma para quem se preocupa com seus bichinhos de
+              verdade. Seja para quem está em busca de um novo amigo ou até mesmo
+              quem precisa, por algum motivo, se desfazer dele. Sabemos que é muito
+              triste quando isso acontece, mas melhor do que abandonar seres tão
+              bondosos, é doá-los para quem pode e quer cuidar deles.
+            </p>
+          </div>
+          <div className="box-about">
+            <h4>Curtiu a idéia? Cadastre-se
+              <Link className="link-about" to="/aboutUs"> aqui.</Link>
+            </h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default AboutUs;
